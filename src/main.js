5741c60bace2b8fce2131b9ffc1b78a171d7173d
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-18 21:03:50 -0300

    feat(validation): implementing the validation of inputs, bug in the imask

--- src/main.js
+++ src/main.js
@@ -19,3 +19,59 @@ function setCardType(type) {
 };
 
 globalThis.setCardType = setCardType;
+
+const securityCode = document.querySelector('#security-code');
+const securityCodePattern = {
+  mask: "0000",
+};
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+const expirationDate = document.querySelector('#expiration-date');
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    }
+  }
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+const cardNumber = document.querySelector('#card-number');
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0, 15}/, 
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0, 2}|^22[2-9]\d|^2[3-7]\d{0, 2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function(appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, '');
+    const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+      return number.match(item.regex);
+    });
+
+    console.log(foundMask);
+
+    return foundMask;
+  },
+};
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+// (^5[1-5]\d{0, 2} | ^22[2-9]\d | ^2[3-7]\d{0, 2})\d{0,12} 
