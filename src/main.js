f2e19e64dc5bf0a2887bc38f922b3c811a4f7bbc
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-19 20:14:09 -0300

    feat(update info): implemented the update of the card informations and finished the project

--- src/main.js
+++ src/main.js
@@ -68,9 +68,54 @@ const cardNumberPattern = {
       return number.match(item.regex);
     });
 
-    console.log(foundMask);
-
     return foundMask;
   },
 };
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+addButton.addEventListener("click", () => {
+  alert("cartão adicionado!");
+});
+
+document.querySelector("form").addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+const cardHolder = document.querySelector('#card-holder');
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value');
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value;
+});
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+});
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value");
+
+  ccSecurity.innerText = code.length === 0 ? "123" : code;
+};
+
+cardNumberMasked.on('accept', () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype;
+  setCardType(cardType);
+  updateCardNumber(cardNumberMasked.value);
+});
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector('.cc-number');
+
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" :  number;
+};
+
+expirationDateMasked.on('accept', () => {
+  updateExpirationDate(expirationDateMasked.value);
+});
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value");
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date;
+};
