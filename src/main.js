b3caf9426e6efc5b566ab7746f17214279fb8099
lucasFilppe <lucasfilipe2323@icloud.com>
2022-10-18 21:16:41 -0300

    trabalhando com a DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+//função para mudar backgroud do cartao
+function setCard(type) {
+  const colors = {
+    visa: ["#436d99", "#2d57f2"],
+    mastercard: ["#df6f29", "#c69347"],
+    default: ["black", "gray"]
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCard("visa")
