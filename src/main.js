0bf6278881da617e144da5b85b56002aab2ebf62
MateusLopes <mateus20.lopes02@gmail.com>
2022-10-20 23:31:47 -0300

    feat: exibindo nome no cartao

--- src/main.js
+++ src/main.js
@@ -72,5 +72,21 @@ const cardNumberPattern = {
     return foundMask
   }
 }
-
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado")
+})
+
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
