f023afaa4604ff057a20249312b522b90fb6bbcf
jvlimadev <jvlima_dev1999@outlook.com>
2022-10-21 17:11:58 -0300

    criando-securitycocde

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -17,3 +18,10 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+//security code
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
