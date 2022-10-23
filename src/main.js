13da3a7053422dae2765bb99bbeac81b4209b4e0
MateusLopes <mateus20.lopes02@gmail.com>
2022-10-23 00:15:54 -0300

    fat: adicionando mascara cartao daredevil

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import "./css/index.css"
 import IMask from "imask"
 
-const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccBgColor = document.querySelector(".cc")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
-    visa: ["#436D99", "#2D57F2"],
-    mastercard: ["#DF6F29", "#C69347"],
-    default: ["black", "grey"]
+    visa: "#1B16FF",
+    mastercard: "#FF4E16",
+    daredevil: "#D00000",
+    default: "#53555E"
   }
 
-  ccBgColor01.setAttribute("fill", colors[type][0])
-  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccBgColor.style.backgroundColor = colors[type]
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
@@ -55,6 +54,11 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^666\d{0,15}/,
+      cardtype: "daredevil"
     },
     {
       mask: "0000 0000 0000 0000",
